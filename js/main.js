3342a20d8ea27ab7a5f5e625ad80ec4400a6049f
Nuffl3s <nuffles0123626@gmail.com>
2023-05-12 22:02:22 +0800

    before going online changes
    optimization

--- js/main.js
+++ js/main.js
@@ -90,3 +90,13 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+const sr =ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.home__image')
+sr.reveal('.home__data', { origin: 'bottom'})
